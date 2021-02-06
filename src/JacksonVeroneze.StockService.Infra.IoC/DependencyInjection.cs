8549f8becc2abe3d891ed6d73a2ea2b59c9d1ebc
Jackson Veroneze <jackson@jacksonveroneze.com>
2021-02-06 19:16:32 -0300

    feat: ajustes na estrutura para rodar a aplicação

---
+++ src/JacksonVeroneze.StockService.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using JacksonVeroneze.StockService.Application.Interfaces;
+using JacksonVeroneze.StockService.Application.Services;
+using JacksonVeroneze.StockService.Core.Communication.Mediator;
+using JacksonVeroneze.StockService.Data.Repositories;
+using JacksonVeroneze.StockService.Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JacksonVeroneze.StockService.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // MediatorHandler
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            // Application
+            services.AddScoped<IProductApplicationService, ProductApplicationService>();
+
+            // Domain
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
---
+++ src/JacksonVeroneze.StockService.Mapper/ProfileMap.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using JacksonVeroneze.StockService.Application.DTO;
+using JacksonVeroneze.StockService.Domain.Entities;
+
+namespace JacksonVeroneze.StockService.Mapper
+{
+    public class ProfileMapStock : Profile
+    {
+        public ProfileMapStock()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+        }
+    }
+}
