9e9835b4751613ae23a9b11168f7c83e046fdeb4
Jackson Veroneze <jackson@jacksonveroneze.com>
2021-04-12 09:36:19 -0300

    feat: realizado ajustes gerais

--- src/JacksonVeroneze.StockService.Api/Util/Logger.cs
+++ src/JacksonVeroneze.StockService.Api/Util/Logger.cs
@@ -10,26 +10,41 @@ namespace JacksonVeroneze.StockService.Api.Util
     {
         public static ILogger FactoryLogger()
         {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory());
-
-            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            if (environment != null && environment.Equals("Development", StringComparison.CurrentCultureIgnoreCase))
-                builder.AddJsonFile("appsettings.json", true, true);
-
-            IConfigurationRoot configuration = builder
-                .AddEnvironmentVariables("APP_CONFIG_")
-                .Build();
-
             return new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
+                .ReadFrom.Configuration(FactoryConfiguration())
                 .WriteTo.Console(
                     outputTemplate:
                     "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
                     theme: AnsiConsoleTheme.Literate)
                 .Enrich.FromLogContext()
                 .CreateLogger();
+        }
+
+        private static IConfigurationRoot FactoryConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory());
+
+            bool isDevelopment = IsDevelopmentEnvironment();
+
+            if (isDevelopment && File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json")))
+                builder.AddJsonFile("appsettings.json", true, true);
+
+            if (isDevelopment &&
+                File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json")))
+                builder.AddJsonFile("appsettings.Development.json", true, true);
+
+            return builder
+                .AddEnvironmentVariables("APP_CONFIG_")
+                .Build();
+        }
+
+        private static bool IsDevelopmentEnvironment()
+        {
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            return environment != null &&
+                   environment.Equals("Development", StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
