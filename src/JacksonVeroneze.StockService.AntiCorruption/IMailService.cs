7cc5bf26d46adedad13359229c954fa8e71fe4c2
Jackson Veroneze <jackson@jacksonveroneze.com>
2021-04-19 22:29:04 -0300

    feat: realizado ajustes gerais

---
+++ src/JacksonVeroneze.StockService.AntiCorruption/IMailService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Refit;
+
+namespace JacksonVeroneze.StockService.AntiCorruption
+{
+    [Headers(new[] {"Accept: application/json;charset=UTF-8"})]
+    public interface IMailService
+    {
+        [Post("/api/v1/mail")]
+        Task<MailResponse> SendAsync(MailRequest request);
+    }
+}
