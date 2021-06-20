8a4d155825f50d265ca9474d03ea321968cdbd79
Jackson Veroneze <jackson@jacksonveroneze.com>
2021-06-20 18:04:21 -0300

    feat: realizado ajustes gerais

--- src/JacksonVeroneze.StockService.Infra.Data/Migrations/20210620210413_Initial.Designer.cs
+++ src/JacksonVeroneze.StockService.Infra.Data/Migrations/20210620210413_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using JacksonVeroneze.StockService.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace JacksonVeroneze.StockService.Infra.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210620210413_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
